43b7b26863cf6a66f7d6963c1c14105ce94b172e
biteme <caro@Ata-MBP.fibertel.com.ar>
2022-10-02 20:11:46 -0300

    fix: pagination

--- src/components/Paginado.jsx
+++ src/components/Paginado.jsx
@@ -4,8 +4,8 @@ import '../styles/Paginado.css';
 export default function Paginado({ countriesPerPage, allCountries, paginado }) {
     const pageNumbers = [];
 
-    for (let i = 0; i <= Math.ceil(allCountries / countriesPerPage); i++) {
-        pageNumbers.push(i + 1);
+    for (let i = 1; i <= Math.ceil(allCountries / countriesPerPage); i++) {
+        pageNumbers.push(i);
     }
 
     return (
